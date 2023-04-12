8d3682d9c5a16510a973ac5f706acc752ebc277c
Joshua <7747985+yeenbean@users.noreply.github.com>
2023-04-12 11:04:26 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Scripts
+
+This repository contains a collection of various scripts, templates, and
+one-liners that I use on a regular basis. I've decided to create this repository
+because I am tired of losing my scripts 😆 but I hope these may help someone
+in the future.
+
+FULL DISCLAIMER: I am no more than an amateur programmer (for now). Please do
+not use these scripts without first auditing the code for yourself!
+
+These scripts come with absolutely no warranty. I am not responsible for any
+damage caused by these scripts--especially when misused.
---
+++ Windows/cmd/Starters/run-as-admin.bat
@@ -0,0 +1,26 @@
+@echo off
+
+
+REM  Check if elevated
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+
+REM If errorlevel is not equal to 0, we are not elevated.
+if '%errorlevel%' NEQ '0' (
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params = %*:"=""
+    echo UAC.ShellExecute "cmd.exe", "/c %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+
+
+REM Add your script here.
