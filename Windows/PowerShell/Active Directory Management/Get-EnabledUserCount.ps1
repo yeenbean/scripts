ec0ee2c692caeb553c6f12c2c1e0352673b0edb4
Joshua <7747985+yeenbean@users.noreply.github.com>
2023-06-26 13:08:18 -0700

    oops this never saved

--- Windows/PowerShell/Active Directory Management/Get-EnabledUserCount.ps1
+++ Windows/PowerShell/Active Directory Management/Get-EnabledUserCount.ps1
@@ -1,1 +1,1 @@
-Get-ADUser -LDAPFilter '(!userAccountControl:1.2.840.113556.1.4.803:=2)' | measure
+Get-ADUser -LDAPFilter '(!userAccountControl:1.2.840.113556.1.4.803:=2)' | measure | Format-Table -AutoSize
